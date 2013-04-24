9c2e76b1f94f076cc6ace326e30cd65a92004873
Wes Hayutin <whayutin@redhat.com>
2013-04-24 18:29:05 -0400

    added rules, and a hello world in index

--- src/app/controllers/splice_reports/filter_controller.rb
+++ src/app/controllers/splice_reports/filter_controller.rb
@@ -2,8 +2,20 @@ module SpliceReports
   
   class FilterController < ::ApplicationController
 
+    def rules
+      read_system = lambda{System.find(params[:id]).readable?}
+        {
+          :index => lambda{true}
+        }
+
+    end
+
     def filter
 
+    end
+
+    def index
+      render :text => 'HELLOOOOO'
     end
 
   end 
