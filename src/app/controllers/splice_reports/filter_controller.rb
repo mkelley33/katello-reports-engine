0e8377438726f74c7fc86a3023c6abca4be9051d
Wes Hayutin <whayutin@redhat.com>
2013-04-25 10:53:44 -0400

    working w/ controller to render a view

--- src/app/controllers/splice_reports/filter_controller.rb
+++ src/app/controllers/splice_reports/filter_controller.rb
@@ -2,9 +2,25 @@ module SpliceReports
   
   class FilterController < ::ApplicationController
 
-    def filter
+    def rules
+      read_system = lambda{System.find(params[:id]).readable?}
+        {
+          :index => lambda{true},
+          :filter => lambda{true}
+        }
 
     end
+
+    def list
+
+    end
+
+
+    def index
+      #render :text => 'HELLOOOOO'
+      render :list
+    end
+
 
   end 
 
