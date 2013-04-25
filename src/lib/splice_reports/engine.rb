096d7027b36b9b44d0f52470815fab8671bc4173
Wes Hayutin <whayutin@redhat.com>
2013-04-25 14:18:31 -0400

    initial commit of naviagion

--- src/lib/splice_reports/engine.rb
+++ src/lib/splice_reports/engine.rb
@@ -1,4 +1,14 @@
 module SpliceReports
   class Engine < ::Rails::Engine
+
+    initializer :finisher_hook do |engine|
+     
+      resources = Dir[File.dirname(__FILE__) + '/navigation/*.rb']
+      resources.uniq.each{ |f| require f }
+ 
+      ::Experimental::Navigation::Additions.insert_after(:import_history, Foo::Navigation::ReportFilter)
+    end
+
+
   end
 end
---
+++ src/lib/splice_reports/navigation/report_filter.rb
@@ -0,0 +1,14 @@
+module SpliceReports
+  module Navigation
+    class ReportFilter < ::Experimental::Navigation::Item
+
+      def initialize(organization)
+        @key           = :special_filters
+        @display       = _("Report Filters")
+        @authorization = lambda{ organization && organization.readable? }
+        @url           = report_filters_path
+      end
+
+    end
+  end
+end
