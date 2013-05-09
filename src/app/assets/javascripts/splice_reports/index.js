28e2300d388944fea92c707ec8a9f70398aea928
whayutin <whayutin@redhat.com>
2013-05-09 08:13:35 -0400

    clean up filter javascript

--- src/app/assets/javascripts/splice_reports/index.js
+++ src/app/assets/javascripts/splice_reports/index.js
@@ -10,27 +10,22 @@ $(document).ready(function() {
 
     KT.panel.set_expand_cb(function(){
 
+        //New Filter: Hide all fields by default
+        $("#hour").hide();
+        $("#daterange").hide();
+        $("#inactive").hide();
+
+        //New Filter: Hide the forms that are not selected
         $("#choose_hour").change(function(){
             $("#hour").show();
             $("#daterange").hide();
             $("#inactive").hide();
         });
 
-        $("#splice_reports_filter_hours").change(function(){
-            $("#splice_reports_filter_inactive").val("nill")
-            $("#splice_reports_filter_start_date").datepicker("setDate", null );
-            $("#splice_reports_filter_end_date").datepicker("setDate", null);
-        });
-    
         $("#choose_daterange").change(function() {
             $("#daterange").show();
             $("#hour").hide();
             $("#inactive").hide();            
-        });
-
-        $("#splice_reports_filter_start_date").change(function(){
-            $("#splice_reports_filter_inactive option:selected").remove();
-            $("#splice_reports_filter_hours option:selected").remove();
         });
 
         $("#choose_inactive").change(function() {
@@ -39,17 +34,27 @@ $(document).ready(function() {
             $("#daterange").hide();            
         });
 
+        //New Filter: If a user sets a value to a different field, reset any previous value in the
+        // the other fields
+        $("#splice_reports_filter_hours").change(function(){
+            $("#splice_reports_filter_inactive").val(-1);
+            $("#splice_reports_filter_start_date").datepicker("setDate", null );
+            $("#splice_reports_filter_end_date").datepicker("setDate", null );
+        });
+    
+        $("#splice_reports_filter_start_date").change(function(){
+            $("#splice_reports_filter_inactive").val(-1);
+            $("#splice_reports_filter_hours").val(-1);
+        });
+
         $("#splice_reports_filter_inactive").change(function(){
-            $("#splice_reports_filter_hours").val("nill");
+            $("#splice_reports_filter_hours").val(-1);
             $("#splice_reports_filter_start_date").datepicker("setDate", null );
             $("#splice_reports_filter_end_date").datepicker("setDate", null);
         });
 
 
-        $("#hour").hide();
-        $("#daterange").hide();
-        $("#inactive").hide();
-
+        //Edit Filter: Hide unused fields
         if (document.getElementById("start_date")){
             if (document.getElementById("start_date").innerHTML != null){
                 $("#daterange".show);
