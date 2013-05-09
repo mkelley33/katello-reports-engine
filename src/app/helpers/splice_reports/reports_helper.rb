9684e3c5e7ce864190a40584e4bb015d8c3a8364
Justin Sherrill <jsherril@redhat.com>
2013-05-09 15:42:19 -0400

    adding link to existing system

--- src/app/helpers/splice_reports/reports_helper.rb
+++ src/app/helpers/splice_reports/reports_helper.rb
@@ -1,8 +1,21 @@
 module SpliceReports
-
   module ReportsHelper
     include SpliceReports::Navigation::RecordMenu
 
-  end
 
+    def find_system(record)
+      uuid = record['instance_identifier']
+      System.where(:uuid=>uuid).first
+    end
+
+    def system_exists(record)
+      !find_system(record).nil?
+    end
+
+    def system_link(record)
+      system = find_system(record)
+      systems_path() + "#!/?item=#{system.id}&search=id:#{system.id}"
+    end
+
+  end
 end
