936fa557640c2e1f62ff45eaae3976721f4eec49
whayutin <whayutin@redhat.com>
2013-05-10 15:02:37 -0400

    added record ident to find_instance_checkins

--- src/app/controllers/splice_reports/reports_controller.rb
+++ src/app/controllers/splice_reports/reports_controller.rb
@@ -197,6 +197,7 @@ module SpliceReports
                            "status",
                            "hostname",
                            "environment",
+                           "record_identifier",
                            "created" ]).as_json
     end
 
