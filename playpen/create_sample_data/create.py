80c29d302f4925022884a43ae19b47c4d76c461b
wes hayutin <whayutin@redhat.com>
2013-06-12 17:52:25 -0400

    updated data load scripts

--- playpen/create_sample_data/create.py
+++ playpen/create_sample_data/create.py
@@ -1,36 +1,53 @@
 #!/usr/bin/python
-import fileinput
-import sys
-import random
-import time
+import calendar
 import datetime
 from datetime import timedelta
+import fileinput
+from optparse import OptionParser
+import os
+import random
+import sys
+
+unique_systems = 0
+parser = OptionParser()
+parser.add_option("-u", "--unique-systems", dest="unique_systems", help="number of unique systems to create")
+(opts, args) = parser.parse_args()
+unique_systems = opts.unique_systems
+print("creating " + str(unique_systems) + " unique systems")
+
+mandatories = ['unique_systems']
+for m in mandatories:
+	if not opts.__dict__[m]:
+		print "mandatory option is missing\n"
+		parser.print_help()
+		exit(-1)
+unique_systems = int(unique_systems) + 1
 
 def replaceAll():
-	old_file = open("sample.json", "r")
-	new_file = open("sample_data_new", "w")
-	old_file_no_breaks = map(lambda line: line.rstrip('\n'), old_file)
+	old_file = open("orig_marketing_product_usage.json", "r")
+	os.remove("marketing_product_usage.json")
+	new_file = open("marketing_product_usage.json", "a")
+	#old_file_no_breaks = map(lambda line: line.rstrip('\n'), old_file)
 
 	orig_line = ""
-	for line in old_file_no_breaks:
+	for i, line in enumerate(old_file):
 		orig_line = line
 
-	status_array = ["current", "invalid", "insufficient"]
-	for x in range(1,103):
-		r = random.randint(0,2)
-		t = random.randint(0,50)
-		print(t)
-		delta = timedelta(hours=int(t))
-		now = datetime.datetime.now()
-		nowish = now - delta
-		gotime = nowish.isoformat()
-		print(gotime)
-		status = status_array[r]
-		line1 = orig_line.replace("server_ident", "server_ident" + str(x))
-		line2 = line1.replace("1000011111", str(int("1000011111") + x))
-		line3 = line2.replace("invalid", status)
-		line4 = line3.replace("change_time", str(gotime))
-		new_file.write(line4)
+		#status_array = ["current", "invalid", "insufficient"]
+		for x in range(1, unique_systems):
+			r = random.randint(0,2)
+			t = random.randint(0,50)
+			delta = timedelta(hours=int(t))
+			now = datetime.datetime.now()
+			nowish = now - delta
+			gotime = calendar.timegm(nowish.timetuple())
+			#status = status_array[r]
+			ident = "server_ident" + str(i + 1) + str(x)
+			print(ident)
+			line1 = orig_line.replace("server_ident", ident)
+			line2 = line1.replace("sat_id", str(int("1000011111") + (i + 1)  + x))
+			line3 = line2.replace("change_time", str(gotime) + "000")
+			new_file.write(line3)
 
 	old_file.close()
 	new_file.close()
---
+++ playpen/create_sample_data/load_data.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+from mongoengine.connection import connect
+from mongoengine import connection, register_connection
+from optparse import OptionParser
+from pymongo import Connection
+from subprocess import call, PIPE
+import os
+
+drop = False
+parser = OptionParser()
+parser.add_option("-d", "--drop", action="store_true", dest="drop", help="drop mpu collection")
+(opts, args) = parser.parse_args()
+drop = opts.drop
+print(drop)
+
+DUMP_DIR = '.'
+
+def setup_database():
+    # Disconnect from the default mongo db, and use a test db instead.
+    
+    conn = Connection()
+    checkin_service = conn["checkin_service"]
+    results = conn["results"]
+      
+    for collection in ['splice_server', 'marketing_product_usage']:
+        if drop:
+          print('DROPPING')
+          checkin_service.drop_collection(collection)
+        print 'importing %s collection' % collection
+        call(['mongoimport', '--db', 'checkin_service', '-c', collection, '--file', 
+              '%s.json' % os.path.join(DUMP_DIR, collection)]
+              )
+    """    
+    for collection in ['marketing_report_data']:
+            results.drop_collection(collection)
+            print 'importing %s collection' % collection
+            call(['mongoimport', '--db', 'results', '-c', collection, '--file', 
+                  '%s.json' % os.path.join(DUMP_DIR, collection)]
+                  ) 
+    """
+
+            
+    conn.disconnect()
+
+    
+
+
+setup_database()
