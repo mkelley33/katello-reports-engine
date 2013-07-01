cfb78d9e84b880d593a90b158371285c053e3711
John Matthews <jwmatthews@gmail.com>
2013-07-01 11:07:24 -0400

    Update so KATELLO_GIT_PATH is consistent with both scripts

--- devel_env/el6/setup_katello_devel_env.sh
+++ devel_env/el6/setup_katello_devel_env.sh
@@ -1,7 +1,7 @@
 # Following: https://fedorahosted.org/katello/wiki/DevelopmentSetup
 
 #Location of Katello git checkout
-export KATELLO_HOME=/katello
+export KATELLO_GIT_PATH=/katello
 
 setenforce 0
 sed -i "s/^SELINUX.*/SELINUX=permissive/" /etc/selinux/config
@@ -28,8 +28,8 @@ service katello-jobs stop; service katello stop
 chkconfig katello-jobs off; chkconfig katello off
 
 # Update katello.yml with development/test DBs
-cp /etc/katello/katello.yml ${KATELLO_HOME}/config/katello.yml
-../update_katello_yml.rb ${KATELLO_HOME}/config/katello.yml
+cp /etc/katello/katello.yml ${KATELLO_GIT_PATH}/config/katello.yml
+../update_katello_yml.rb ${KATELLO_GIT_PATH}/config/katello.yml
 
 # Modify DB access
 cat >/var/lib/pgsql/data/pg_hba.conf <<EOF
@@ -41,12 +41,12 @@ EOF
 
 service postgresql restart
 
-scl enable ruby193 "cd ${KATELLO_HOME} && bundle install"
-cd ${KATELLO_HOME} && echo "yes" | scl enable ruby193 "./script/katello-reset-dbs development ."
+scl enable ruby193 "cd ${KATELLO_GIT_PATH} && bundle install"
+cd ${KATELLO_GIT_PATH} && echo "yes" | scl enable ruby193 "./script/katello-reset-dbs development ."
 echo "Katello devel enviroment is setup"
 echo "Please run the below to bring up the Katello Development Server:"
 echo " ** If setting this up in VM, ensure you ssh into the VM prior to running below**"
-echo " 1) cd ${KATELLO_HOME}"
+echo " 1) cd ${KATELLO_GIT_PATH}"
 echo " 2) sudo scl enable ruby193 'rails s'"
 echo "Enjoy."
 
