a14c71009394494dd5455faedfe3514caa659e8e
John Matthews <jwmatthews@gmail.com>
2013-07-02 09:28:02 -0400

    Fix syntax issue with clear splice script

--- devel_env/el6/clear_prior_splice_install.sh
+++ devel_env/el6/clear_prior_splice_install.sh
@@ -1,6 +1,6 @@
 source ./env_vars
 
-if [ -f ${KATELLO_GIT_PATH}/bundler.d/splice_reports.rb ];
+if [ -f ${KATELLO_GIT_PATH}/bundler.d/splice_reports.rb ]; then
     echo "Removing prior bundler.d/splice_reports.rb install"
     rm ${KATELLO_GIT_PATH}/bundler.d/splice_reports.rb
 fi
