01dfc4827c6e15c6a918366a8236802ba59e03b8
Wes Hayutin <whayutin@redhat.com>
2013-07-26 11:13:23 -0400

    unable to get this working w/o two clicks.. but adding selected class from katello vs underline

--- src/app/assets/javascripts/splice_reports/report_index.js
+++ src/app/assets/javascripts/splice_reports/report_index.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-
-  $("body").on('click', 'li.panel_link', function() {
-    console.log('click panel');
-    $(this.firstChild).css("text-decoration", "underline");
-  });
+//$(document).ready(function() {
+  
+//wait for page to finish loading
+$(window).bind("load", function() {
+    $("body").on('click', 'li.panel_link', function( e ) {
+      console.log('click panel');
+      //unable to stop details panel from refreshing twice
+      //e.preventDefault();
+      //e.stopPropagation();
+      //e.stopImmediatePropagation();
+      //$(this.firstChild).css("text-decoration", "underline");
+      $(this.firstChild).addClass("selected");
+    });
   
   $("#filter_tip").click(function() {
     $("#description").show();
