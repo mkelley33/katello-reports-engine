ff2abe509ca349a2a30bb43dd9b93185f233de4a
Chris Duryee (beav) <cduryee@redhat.com>
2013-08-08 14:42:19 -0400

    995132: remove double-validation of some form fields

--- src/app/models/splice_reports/filter.rb
+++ src/app/models/splice_reports/filter.rb
@@ -19,8 +19,6 @@ module SpliceReports
     has_and_belongs_to_many :organizations, :join_table => 'splice_reports_filters_organizations', :foreign_key=>'splice_reports_filter_id'
     belongs_to :user
 
-    validates :name, :presence => true
-    validates :status, :presence => true
     validates_with Validators::KatelloNameFormatValidator, :attributes => :name
     validates_with Validators::KatelloDescriptionFormatValidator, :attributes => :description
 
